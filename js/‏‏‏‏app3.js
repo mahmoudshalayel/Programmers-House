4ad85e908d61299dd6ad8ca52d2429b486a5a4e2
mahmoudshalayel <mahmoudshalayel0@gmail.com>
2023-08-27 22:02:17 +0300

    Programmers House

---
+++ js/‏‏‏‏app3.js
@@ -0,0 +1,7 @@
+function mycontent(){
+
+    var myH1= document.getElementById("d1");
+
+    myH1.innerHTML=  "<br>" + "My Number in Whats App : <span> +970598142927 </span> "  + "<br>"  + "<br>"  +  
+    "My Account on Facebook :<span> Mahmoud Shalayel </span> " ;
+}
